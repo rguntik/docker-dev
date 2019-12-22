fee0dc32cbdfa5155eba065f6b211540d6b60f15
rguntik <rguntik@gmail.com>
2019-12-22 11:16:38 +0200

    hosts authochanging

--- toolbox.sh
+++ toolbox.sh
@@ -4,18 +4,22 @@ export HOME_DIR=$(pwd)
 cd ../sites
 export VOLUME_DIR=$(pwd)
 cd $HOME_DIR
+export HOST_IP="172.123.0.1"
 
 cd $(dirname $0)
 export CURRENT_UID=$(id -u):$(id -g)
 
 function build() {
-    FOLDERS=$(ls -d */ | cut -f1 -d'/')
-
+    cd $VOLUME_DIR
     HTTPD_FILE="$HOME_DIR/httpd/httpd.conf"
     NGINX_FILE="$HOME_DIR/nginx/nginx.conf"
 
     cat "$HTTPD_FILE.template" > $HTTPD_FILE
     cat "$NGINX_FILE.template" > $NGINX_FILE
+
+    cat /etc/hosts | grep -v $HOST_IP >"$HOME_DIR/hosts"
+    echo "$HOST_IP dev.loc:8080" >>"$HOME_DIR/hosts"
+    echo "$HOST_IP dev.loc" >>"$HOME_DIR/hosts"
 
     for dir in $(ls -d */ | cut -f1 -d'/'); do
         export SERVER_NAME="$dir.dev.loc"
@@ -23,12 +27,22 @@ function build() {
 
         envsubst '${SERVER_NAME}, ${SITE_DIR}' <"$HOME_DIR/httpd/httpd.conf.vhost.template" >> $HTTPD_FILE
         envsubst '${SERVER_NAME}, ${SITE_DIR}' <"$HOME_DIR/nginx/nginx.conf.server.template" >> $NGINX_FILE
-        echo "}" >> $NGINX_FILE
+
+        echo "$HOST_IP $SERVER_NAME" >>"$HOME_DIR/hosts"
     done;
+    echo "}" >> $NGINX_FILE
+
+    cat "$HOME_DIR/hosts" | sudo tee /etc/hosts
+    echo "<?php echo '" >"$VOLUME_DIR/index.php"
+    cat "$HOME_DIR/hosts" | grep $HOST_IP | awk '{print "http://" $2 "<br>"}' >>"$VOLUME_DIR/index.php"
+    echo "';" >>"$VOLUME_DIR/index.php"
+    rm "$HOME_DIR/hosts"
+
 }
 
 function docker_up() {
     build
+    cd $HOME_DIR
     docker-compose -f docker-compose.yml up $@
 }
 
